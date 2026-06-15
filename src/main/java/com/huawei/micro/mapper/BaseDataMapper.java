a1bed29f4ab3c8ff400305956a965461bce97eb8
EricDing0626 <ericding626@gmail.com>
2026-06-15 15:21:30 +0800

    feat: 新增 OperateLog/BaseData Mapper 并配置 AOP 与 Cache 依赖

---
+++ src/main/java/com/huawei/micro/mapper/BaseDataMapper.java
@@ -0,0 +1,13 @@
+package com.huawei.micro.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.huawei.micro.entity.BaseData;
+
+/**
+ * 基础数据数据访问接口。
+ *
+ * @author Eric
+ * @since 1.0.0
+ */
+public interface BaseDataMapper extends BaseMapper<BaseData> {
+}
---
+++ src/main/java/com/huawei/micro/mapper/OperateLogMapper.java
@@ -0,0 +1,13 @@
+package com.huawei.micro.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.huawei.micro.entity.OperateLog;
+
+/**
+ * 操作日志数据访问接口。
+ *
+ * @author Eric
+ * @since 1.0.0
+ */
+public interface OperateLogMapper extends BaseMapper<OperateLog> {
+}
